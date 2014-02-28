cb48029bb2f7bea2c6172124e746bdbe23c4cba8
Peter Smith <psmith@sensiolabs.co.uk>
2014-02-28 11:06:16 +0000

    Add specs missed from last commit

---
+++ spec/Orukusaki/Bundle/SlackBundle/Event/CommandRecievedEventSpec.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace spec\Orukusaki\Bundle\SlackBundle\Event;
+
+use PhpSpec\ObjectBehavior;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+class CommandRecievedEventSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $request = new ParameterBag;
+        $request->set('token', '12345678');
+        $request->set('team_id', 'team');
+        $request->set('channel_id', 'CX12345');
+        $request->set('channel_name', 'My Channel');
+        $request->set('user_id', 'user_1234');
+        $request->set('user_name', 'myName');
+        $request->set('command', '/slash');
+        $request->set('text', 'command');
+
+        $this->beConstructedWith($request);
+    }
+
+    public function it_is_an_event()
+    {
+        $this->shouldHaveType('Symfony\Component\EventDispatcher\Event');
+    }
+
+    public function it_stops_propagation_when_response_set()
+    {
+        $this->isPropagationStopped()->shouldReturn(false);
+        $this->setResponse('A nice response');
+        $this->isPropagationStopped()->shouldReturn(true);
+        $this->getResponse()->shouldReturn('A nice response');
+    }
+
+   public function it_exposes_a_command()
+    {
+        // var_dump($this->getCommand());
+        $this->getCommand()->shouldReturn(
+            [
+                'token'        => '12345678',
+                'team_id'      => 'team',
+                'channel_id'   => 'CX12345',
+                'channel_name' => 'My Channel',
+                'user_id'      => 'user_1234',
+                'user_name'    => 'myName',
+                'command'      => '/slash',
+                'text'         => 'command',
+            ]
+        );
+    }
+}
---
+++ spec/Orukusaki/Bundle/SlackBundle/Event/MessageRecievedEventSpec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec\Orukusaki\Bundle\SlackBundle\Event;
+
+use PhpSpec\ObjectBehavior;
+
+class MessageRecievedEventSpec extends ObjectBehavior
+{
+    public function it_is_an_event()
+    {
+        $this->shouldHaveType('Symfony\Component\EventDispatcher\Event');
+    }
+
+    public function let()
+    {
+        $this->beConstructedWith(array('text' => 'hi'), 'CK12345');
+    }
+
+    public function it_should_expose_message_and_channel()
+    {
+        $this->getMessage()->shouldReturn(array('text' => 'hi'));
+        $this->getChannel()->shouldReturn('CK12345');
+    }
+}
